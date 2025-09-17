c7841e5e04152d00876b8a54802e90cc5c081d65
Gastón <gastonibanezcv@gmail.com>
2025-09-17 02:08:57 -0300

    Creacion de componentes y Registrar Usuario / Modificaciones de HTTPServicio

--- Proyecto2024.Client/Program.cs
+++ Proyecto2024.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Proyecto2024.Client;
+using Proyecto2024.Client.Autorizacion;
 using Proyecto2024.Client.Layout.Autorizacion;
 using Proyecto2024.Client.Servicios;
 
@@ -13,7 +14,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<IHttpServicio, HttpServicio>();
+
+// Registramos el proveedor de autenticacion JWT
+builder.Services.AddScoped<ProveedorAutenticacionJWT>();
+// al compilar el programa ahora se ejecuta el proveedor de autenticacion - Cambiamos el objeto por ProveedorAutenticacionJWT que es el que hemos creado
+builder.Services.AddScoped<AuthenticationStateProvider, ProveedorAutenticacionJWT>(proveedor =>
+    proveedor.GetRequiredService<ProveedorAutenticacionJWT>());
 // al compilar el programa ahora se ejecuta el proveedor de autenticacion
-builder.Services.AddScoped<AuthenticationStateProvider, ProveedorAutenticacion>();
+builder.Services.AddScoped<ILoginService, ProveedorAutenticacionJWT>(proveedor =>
+    proveedor.GetRequiredService<ProveedorAutenticacionJWT>());
+
 
 await builder.Build().RunAsync();
